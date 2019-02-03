3df4602e8546ff5a4b900e0afbf774a9710e5309
Rob Brand <spikeerob@googlemail.com>
2019-02-03 11:44:25 +0000

    Rename BlazorWeb => Web

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace BlazorWeb
+namespace Web
 {
     public class Program
     {
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazorWeb
+namespace Web
 {
     public class Startup
     {
